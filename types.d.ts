13e74063cc7b5d63909f0c5723251b68e4fb2bd7
Koji Eguchi <koji.eguchi@hotmail.com>
2023-05-10 12:14:13 -0400

    first commit

---
+++ types.d.ts
@@ -0,0 +1,16 @@
+interface Result {
+  pageid: string,
+  title: string,
+  extract: string,
+  thumbnail?: {
+      source: string,
+      width: number,
+      height: number,
+  }
+}
+
+interface SearchResult {
+  query?: {
+      pages?: Result[],
+  },
+}
